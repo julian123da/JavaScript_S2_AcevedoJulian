fe54e5aac706c7ed73a566f7a0410bb576a9979b
julian123da <julianacevedo326@gmail.com>
2025-07-18 12:46:11 -0500

    feat: :sparkles: Beginning of JavaScript

---
+++ Dia1/scripts/scripts.js
@@ -0,0 +1,50 @@
+// #########################
+// ##### DIA 1 #####
+// #########################
+
+//¡¡¡Primer comando!!!!
+console.log("Hola Mundo!");
+
+// #########################
+// ##### DATA PRIMITIVA #####
+// #########################
+//Number
+variable1= 3.14;
+console.log(variable1);
+console.log(typeof (variable1));
+
+//Concatenación
+palabara1="Campus";
+palabra2="Lands";
+palabra3=palabara1+palabra2;
+console.log(palabra3);
+
+
+nombrePersona = "Luis Angel";
+plataActual=55000;
+console.log("La persona llamada ",nombrePersona," tiene en su cuenta bancaria ",plataActual, " pesos colombianos!");
+
+//Booleanos
+booleanito1=true;
+booleanito2=false;
+console.log(typeof booleanito1);
+
+//Condicionales
+if(booleanito1 == true){
+    console.log("Es verdadero!!");
+}else{
+    console.log("No es verdadero :sadfeis:");
+}
+
+//Bucles
+for(i=0;i<4;i=i+1){
+    console.log("Wenas");
+}
+
+holi = true;
+while(holi==true){
+     console.log("Wenass");
+     holi=false;
+}
+
+// Desarrollado por : Julian David Acevedo Gómez T.I: 1.097.100.290
