5090e77559d6efc445f57824f5a5bda1746ad705
julian123da <julianacevedo326@gmail.com>
2025-07-23 11:14:58 -0500

    feat: :sparkles: Quiz

---
+++ Dia4_Quiz_JavaScript/scripts/scripts.js
@@ -0,0 +1,93 @@
+let Hamburguesería  = [];
+let continuar = true;
+
+let JSONIngredientes 
+[
+    {
+        "nombre": "Pan",
+        "descripcion": "Pan de hamburguesa clásico",
+        "precio": 2.5,
+        "stock": 500
+    },
+    {
+        "nombre": "Carne de res",
+        "descripcion": "Carne de res jugosa y sabrosa",
+        "precio": 8,
+        "stock": 300
+    },
+    {
+        "nombre": "Queso cheddar",
+        "descripcion": "Queso cheddar derretido",
+        "precio": 1.5,
+        "stock": 200
+    }
+]
+
+let JSONCategorías
+
+[
+    {
+        "nombre": "Clásica",
+        "descripcion": "Hamburguesas clásicas y sabrosas"
+    },
+    {
+        "nombre": "Vegetariana",
+        "descripcion": "Hamburguesas sin carne, perfectas para vegetarianos"
+    },
+    {
+        "nombre": "Gourmet",
+        "descripcion": "Hamburguesas gourmet con ingredientes premium"
+    }
+]
+
+let JSONHamburguesas
+[
+    {
+        "nombre": "Clásica",
+        "categoria": "Clásica",
+        "ingredientes": ["Pan", "Carne de res", "Queso cheddar", "Lechuga", "Tomate", "Cebolla", "Mayonesa", "Ketchup"],
+        "precio": 10,
+        "chef": "ChefA"
+    },
+    {
+        "nombre": "Vegetariana",
+        "categoria": "Vegetariana",
+        "ingredientes": ["Pan integral", "Hamburguesa de lentejas", "Queso suizo", "Espinacas", "Cebolla morada", "Aguacate", "Mayonesa vegana"],
+        "precio": 8,
+        "chef": "ChefB"
+    },
+    {
+        "nombre": "Doble Carne",
+        "categoria": "Gourmet",
+        "ingredientes": ["Pan de sésamo", "Doble carne de res", "Queso cheddar", "Bacon", "Lechuga", "Cebolla caramelizada", "Salsa BBQ"],
+        "precio": 12,
+        "chef": "ChefC"
+    }
+]
+
+let JSONChefs
+[
+    {
+        "nombre": "ChefA",
+        "especialidad": "Carnes"
+    },
+    {
+        "nombre": "ChefB",
+        "especialidad": "Cocina Vegetariana"
+    },
+    {
+        "nombre": "ChefC",
+        "especialidad": "Gourmet"
+    }
+]
+
+while (Hamburguesas == true) {
+    let 
+
+}
+
+
+encontrar
+agregar
+actualizar
+leer
