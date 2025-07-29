4049b39ea831ecf75010d6997e43311f3a4628f8
julian123da <julianacevedo326@gmail.com>
2025-07-28 20:01:24 -0500

    feat: :sparkles: Work API

--- Dia6/script/script.js
+++ Dia6/script/script.js
@@ -1,3 +1,25 @@
 function getCharacters(done) {
 
+    const results = fetch("https://rickandmortyapi.com/api/character");
+
+    results
+         .then(Response => Response.json()) 
+         .then(data => {
+            done(data)
+         });
+    
 }
+
+getCharacters(data => {
+    data.results.forEach(Personaje => {
+        
+
+       const article = document.createRange().createContextualFragment(/*html*/
+       
+       ´);
+
+
+    });
+
+
+});
