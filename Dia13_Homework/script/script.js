509805195cebbf3144a0586cb7b6419053550efc
julian123da <julianacevedo326@gmail.com>
2025-08-05 19:36:27 -0500

    feat: :sparkles: SuperHero

--- Dia13_Homework/script/script.js
+++ Dia13_Homework/script/script.js
@@ -1,39 +1,10 @@
-let currentPokemon = 1;
-const image = document.getElementById("pokemonImage");
-const nameEl = document.getElementById("pokemonName");
-const idEl = document.getElementById("pokemonId");
-const input = document.getElementById("searchInput");
+const BASE_URL = "https://www.superheroapi.com/api.php/7f07b2080b6fcedb6525374cc69a13a2/";
 
-function fetchPokemon(identifier) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${identifier.toLowerCase()}`)
-    .then(response => {
-      if (!response.ok) throw new Error("No encontrado");
-      return response.json();
-    })
-    .then(data => {
-      currentPokemon = data.id;
-      image.src = data.sprites.other['official-artwork'].front_default || '';
-      nameEl.textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-      idEl.textContent = `#${String(data.id).padStart(3, "0")}`;
-    })
-    .catch(err => {
-      image.src = "";
-      nameEl.textContent = "Not found";
-      idEl.textContent = "#???";
-    });
-}
-
-function changePokemon(direction) {
-  currentPokemon += direction;
-  if (currentPokemon < 1) currentPokemon = 1;
-  fetchPokemon(currentPokemon);
-}
-
-input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    const value = input.value.trim();
-    if (value !== "") fetchPokemon(value);
-    input.value = "";
+function getHeroId(){
+  const id = document.getElementByI("heroId").value;
+  if (!id || id < 1 || id > 731) {
+    alert("Introduzca con ID valido entre 1 y 731.");
+    return null;
   }
-});
-fetchPokemon(currentPokemon);
+  return id; 
+}
