07d7c1147f1fc9e44fe099a943ee6da581fcc8fa
julian123da <julianacevedo326@gmail.com>
2025-08-12 11:50:18 -0500

    feat: :sparkles: Practica

--- Dia16/script/script.js
+++ Dia16/script/script.js
@@ -8,16 +8,46 @@ document.addEventListener('DOMContentLoaded',()=>{
         let data = await res.json();
         return data;
     }
-    //console.log(fetchData());
-    function displayCapsula(capsula){
-        datosContenedeor.innerHTML='';
-        capsula.forEach(cap =>{
-            const capDiv = document.createElement('div')
-            if(cap.status==="ready"){
-                capDiv.classList.add('capsulaNegativa');
-                capDiv.innerHTML = ''
-            }
-        });
-    }
-    displayCapsula();
+   //console.log(fetchData());
+   function displayCapsula(capsula){
+    datosContenedor.innerHTML='';
+    capsula.forEach(cap =>{
+        const capDiv = document.createElement('div')
+        if(cap.status==="ready"){
+            capDiv.classList.add('capsulaNegativa');
+            capDiv.innerHTML =`
+            <div class="infoTextNegativo">
+            <p>${cap["task"]}</p>
+        </div>
+        <div class="botones">
+            <div class="terminadoNegativo">
+                <img src="./storage/img/pngwing.com (2).png" alt="">
+            </div>
+            <div class="eliminadoNegativo">
+                <img src="./storage/img/pngwing.com (4).png" alt="">
+            </div>
+        </div>
+            `
+        }else if(cap.status==="On hold"){
+            capDiv.classList.add('capsula');
+            capDiv.innerHTML=`
+             <div class="capsula">
+        <div class="infoText">
+            <p>${cap["task"]}</p>
+        </div>
+        <div class="botones">
+            <div class="terminado">
+                <img src="./storage/img/pngwing.com (2).png" alt="">
+            </div>
+            <div class="eliminado">
+                <img src="./storage/img/pngwing.com (4).png" alt="">
+            </div>
+        </div>`
+        }
+        datosContenedor.appendChild(capDiv);
+    });
+}
+fetchData().then(data =>{
+    displayCapsula(data);
+})
 });
