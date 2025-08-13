701cc9498778c598487002a29c0d9f50f46ff6c7
julian123da <julianacevedo326@gmail.com>
2025-08-13 11:35:33 -0500

    feat: :sparkles: practica

--- Dia16/script/script.js
+++ Dia16/script/script.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    const datosContenedeor = document.querySelector('.opciones');
-    const taskInput = document.getElementById('taskINput');
+    const datosContenedor = document.querySelector('.opciones');
+    const taskInput = document.getElementById('taskInput');
     const addTaskButton = document.getElementById('addTaskButton');
 
     async function fetchData(){
-        const res = await fetch('https://689a16b7fed141b96ba1d20f.mockapi.io/data/Users');
+        const res =  await fetch('https://66df3340de4426916ee3dd7e.mockapi.io/tareas',{
+            method: 'GET',
+            headers :{
+                'Content-Type':'application/json'
+            }
+        });
+
+
         let data = await res.json();
         return data;
     }
 
-    //################
-    //DANGER ZONE
-
-    //################
     async function addNewTask(){
-        const task = taskINput.value;
+        const task = taskInput.value;
         console.log(task);
         if (task.trim()==='') return;
-        await fetch('https://689a16b7fed141b96ba1d20f.mockapi.io/data/Users',{
+        await fetch('https://66df3340de4426916ee3dd7e.mockapi.io/tareas',{
             method: 'POST',
             headers :{
                 'Content-Type':'application/json'
@@ -31,48 +34,59 @@ document.addEventListener('DOMContentLoaded',()=>{
         displayCapsula(data);
     }
 
-
-   //console.log(fetchData());
-   function displayCapsula(capsula){
-    datosContenedor.innerHTML='';
-    capsula.forEach(cap =>{
-        const capDiv = document.createElement('div')
-        if(cap.status==="ready"){
-            capDiv.classList.add('capsulaNegativa');
-            capDiv.innerHTML =`
-            <div class="infoTextNegativo">
-            <p>${cap["task"]}</p>
-        </div>
-        <div class="botones">
-            <div class="terminadoNegativo">
-                <img src="./storage/img/pngwing.com (2).png" alt="">
+    //console.log(fetchData());
+    function displayCapsula(capsula){
+        datosContenedor.innerHTML='';
+        capsula.forEach(cap =>{
+            const capDiv = document.createElement('div')
+            if(cap.status==="ready"){
+                capDiv.classList.add('capsulaNegativa');
+                capDiv.innerHTML =`
+                <div class="infoTextNegativo">
+                <p>${cap["task"]}</p>
             </div>
-            <div class="eliminadoNegativo">
-                <img src="./storage/img/pngwing.com (4).png" alt="">
+            <div class="botones">
+                <div class="terminadoNegativo">
+                    <img src="./storage/img/pngwing.com (2).png" data-id="${cap["id"]}" alt="" class='completado'>
+                </div>
+                <div class="eliminadoNegativo">
+                    <img src="./storage/img/pngwing.com (4).png" data-id="${cap["id"]}" alt="" class='eliminado'>
+                </div>
             </div>
-        </div>
-            `
-        }else if(cap.status==="On hold"){
-            capDiv.classList.add('capsula');
-            capDiv.innerHTML=`
-             <div class="capsula">
-        <div class="infoText">
-            <p>${cap["task"]}</p>
-        </div>
-        <div class="botones">
-            <div class="terminado">
-                <img src="./storage/img/pngwing.com (2).png" alt="">
+                `
+            }else if(cap.status==="On hold"){
+                capDiv.classList.add('capsula');
+                capDiv.innerHTML=`
+                 <div class="capsula">
+            <div class="infoText">
+                <p>${cap["task"]}</p>
             </div>
-            <div class="eliminado">
-                <img src="./storage/img/pngwing.com (4).png" alt="">
-            </div>
-        </div>`
-        }
-        datosContenedor.appendChild(capDiv);
+            <div class="botones">
+                <div class="terminado">
+                    <img src="./storage/img/pngwing.com (2).png" data-id="${cap["id"]}" alt="" class='completado'>
+                </div>
+                <div class="eliminar">
+                    <img src="./storage/img/pngwing.com (4).png" data-id="${cap["id"]}" alt="" class='eliminado'>
+                </div>
+            </div>`
+            }
+            datosContenedor.appendChild(capDiv);
+        });
+        document.querySelectorAll('.completado').forEach(button => {
+                button.addEventListener('click',botonCompletado);
+        });
+        document.querySelectorAll('.eliminado').forEach(button => {
+                button.addEventListener('click',botonEliminado);
+        });
+    }
+    fetchData().then(data =>{
+        displayCapsula(data);
     });
-}
-fetchData().then(data =>{
-    displayCapsula(data);
-});
-addTaskButton.addEventListener('click',addNewTask());
+    addTaskButton.addEventListener('click',addNewTask);
+    function botonCompletado(){
+        const id =(event.target.getAttribute());
+    }
+    function botonEliminado(){
+        console.log(event.target.getAttribute('data-id'));
+    }
 });
